83bd24533da1ec751c8df6506870233d23be4208
Gaëtan Serré <gaetan.serre@universite-paris-saclay.fr>
2023-01-21 21:04:46 +0100

    Fix Lipschitz rosenbrock

--- rosenbrock.py
+++ rosenbrock.py
@@ -3,7 +3,7 @@ import numpy as np
 class Function:
   def __init__(self) -> None:
     self.bounds = np.array([(-3, 3), (-3, 3)])
-    self.k = 7304
+    self.k = 14607
     
   def __call__(self, x: np.ndarray) -> float:
     return -(100 * np.square(x[1] - np.square(x[0])) + np.square(1 - x[0]))
