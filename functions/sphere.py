2fbb30d891125530e75a6d48f8d8ce9c62ae8716
Gaëtan Serré <gaetan.serre@universite-paris-saclay.fr>
2023-01-22 23:24:07 +0100

    Sphere function

---
+++ functions/sphere.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class Function:
+  def __init__(self) -> None:
+    self.bounds = np.array([(0, 1), (0, 1)])
+    self.k = 1.5
+    
+  def __call__(self, x: np.ndarray) -> float:
+    return -np.sqrt( (x[0] - np.pi / 16)**2 + (x[1]-np.pi/16)**2 )
--- gen_figures.sh
+++ gen_figures.sh
@@ -6,4 +6,5 @@ python main.py --function himmelblau.py -n 2000 -r 2 && \
 python main.py --function functions/holder.py -n 2000 -r 2 && \
 python main.py --function functions/rastrigin.py -n 1000 -r 2 && \
 python main.py --function functions/rosenbrock.py -n 5000 -r 2 && \
+python main.py --function functions/sphere.py -n 30 -r 2 && \
 python main.py --function square.py -n 500 -r 2
