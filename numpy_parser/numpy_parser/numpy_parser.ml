d413b3c468c7dd074e042d05a6dd90028bb7a39d
Gaëtan Serré <gaetan.serre@universite-paris-saclay.fr>
2023-01-28 21:01:57 +0100

    Delete numpy parser

--- numpy_parser/numpy_parser/numpy_parser.ml
+++
@@ -1,9 +0,0 @@
-open Printf
-
-let _ =
-  
-  let fun_str = Sys.argv.(1) in
-  let lexbuf = Lexing.from_string fun_str in
-  let fun_expr = Parser.function_expr Lexer.token lexbuf in
-
-  printf "Parsed\n"
