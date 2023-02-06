9190acb89a91a11512b4c20c8ad7b3a020366dd7
Gaëtan Serré <gaetan.serre@universite-paris-saclay.fr>
2023-02-06 14:40:46 +0100

    Update script

--- gen_figures.sh
+++ gen_figures.sh
@@ -3,9 +3,9 @@
 set -x
 
 python main.py --function himmelblau.py -n 2000 -r 1 && \
-python main.py --function functions/holder.py -n 2000 -r 1 && \
-python main.py --function functions/rastrigin.py -n 1000 -r 1 && \
-python main.py --function functions/rng.py -n 500 -r 1 && \
-python main.py --function functions/rosenbrock.py -n 2000 -r 1 && \
-python main.py --function functions/sphere.py -n 30 -r 1 && \
+python main.py --function holder.py -n 2000 -r 1 && \
+python main.py --function rastrigin.py -n 1000 -r 1 && \
+python main.py --function rng.py -n 500 -r 1 && \
+python main.py --function rosenbrock.py -n 2000 -r 1 && \
+python main.py --function sphere.py -n 30 -r 1 && \
 python main.py --function square.py -n 500 -r 1
